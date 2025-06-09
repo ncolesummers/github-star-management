1d902063c2ecfdd3afe604511334a491033da0a4
Nathan Summers <nsummers72@gmail.com>
2025-06-08 19:59:49 -0700

    chore: remove test files for pre-push hook validation

--- src/utils/no-docs-update.ts
+++
@@ -1,7 +0,0 @@
-/**
- * This file is created to test the pre-push hook
- * when no documentation is updated.
- */
-export function testFunction(): string {
-  return "This should trigger the pre-push hook";
-}
